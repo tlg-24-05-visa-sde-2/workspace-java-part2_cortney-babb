11e2e16daf5367de59960a882cb6a6a3dee38c15
Cortney B <cortneyb@tlgcohort.com>
2024-07-18 14:22:07 -0400

    work with jnuit testing

--- Lab03.1/test/com/math/CalculatorTest.java
+++ Lab03.1/test/com/math/CalculatorTest.java
@@ -9,26 +9,46 @@
 package com.math;
 
 import static org.junit.Assert.*;
-import org.junit.Test;
+
+import org.junit.*;
 
 public class CalculatorTest {
+    // business object(s) under test - call "fixture"
+    private Calculator calc;
+
+    @BeforeClass
+    public static void beforeClass() {
+        System.out.println("Before Class");
+    }
+
+    @Before
+    public void setUp() {
+        calc = new Calculator();
+    }
+
+    @After
+    public void tearDown() {
+        calc = null;
+    }
+
+    @AfterClass
+    public static void afterClass() {
+        System.out.println("After Class");
+    }
 
     @Test
     public void testIsEven() {
-        Calculator calc = new Calculator();
         assertTrue(calc.isEven(6));
         assertFalse(calc.isEven(11));
     }
 
     @Test
     public void testDivide() {
-        Calculator calc = new Calculator();
         assertEquals(6.5, calc.divide(13, 2), .001); // decimals must use delta (.001)
     }
 
     @Test
     public void testAdd() {
-        Calculator calc = new Calculator();
         assertEquals(5, calc.add(1, 4));  // expected, actual
     }
 }

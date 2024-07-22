80876160287d728e8c8d4b8d833d329a099ac4c5
Cortney B <cortneyb@tlgcohort.com>
2024-07-22 15:18:29 -0400

    Created a generic class extending Products to MusicItem and MediaItem

--- Lab04.2/src/com/javatunes/product/Order.java
+++ Lab04.2/src/com/javatunes/product/Order.java
@@ -18,12 +18,16 @@ public class Order {
     }
 
     /**
-     * TODO:
      * get the items from the cart and iterate over them, print each item's product code
      * get cart total and print
      */
-    public void processCart(Object reference_to_cart) {
+    public void processCart(ShoppingCart<? extends Product> cart) {
+        System.out.println(getId() + " contains the following: ");
 
+        for (Product product : cart.allItems()) {
+            System.out.println(product.getCode());
+        }
+        System.out.println("Order Total: " + cart.total());
     }
 
     public String getId() {
--- Lab04.2/src/com/javatunes/product/ShoppingCart.java
+++ Lab04.2/src/com/javatunes/product/ShoppingCart.java
@@ -12,9 +12,20 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
 
-public class ShoppingCart<T> {
+/*
+ * Clients will create ShoppingCart<Product>, ShoppingCart<MusicItem>, or ShoppingCart<MediaPlayer>
+ */
+public class ShoppingCart<T extends Product> {
     // storage for the cart's contents
     private Collection<T> items = new ArrayList<T>();  // diamond not used here just to emphasize the T
+
+    public int size() {
+        return items.size();
+    }
+    // return read-only view of collection
+    public Collection<T> allItems() {
+        return Collections.unmodifiableCollection(items);
+    }
 
     public void addItem(T item) {
         items.add(item);
@@ -22,5 +33,13 @@ public class ShoppingCart<T> {
 
     public void removeItem(T item) {
         items.remove(item);
+    }
+
+    public double total() {
+        double result = 0.0;
+        for (T item : items) {
+            result += item.getPrice();
+        }
+        return result;
     }
 }

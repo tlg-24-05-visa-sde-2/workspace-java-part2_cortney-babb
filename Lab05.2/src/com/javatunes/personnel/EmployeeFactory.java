55806b4e8861bc2a4d817c42ef602c6ecc8d38a6
Cortney B <cortneyb@tlgcohort.com>
2024-07-24 16:09:49 -0400

    refactored EmployeeFactory

--- Lab05.2/src/com/javatunes/personnel/EmployeeFactory.java
+++ Lab05.2/src/com/javatunes/personnel/EmployeeFactory.java
@@ -19,11 +19,36 @@ public class EmployeeFactory {
 
     /**
      * TODO: given the input map, create and return the correct object (with its properties set).
-     * If the input map's "type" value is not "HE" or "SE", throw IllegalArgumentException with a suitable message.
+     * If the input map's "type" value is not "HE" or "SE",
+     * throw IllegalArgumentException with a suitable message.
      */
     public static Employee createEmployee(Map<String,String> inputMap) throws IllegalArgumentException {
         // return value
         Employee emp = null;
+
+        String type = inputMap.get("type");
+
+        if (!"SE".equals(type) && !"HE".equals(type)) {
+            throw new IllegalArgumentException("Invalid Employee Type: " + type + " is not recognized." +
+                    " Must be [SE]Salaried Employee" +
+                    " or [HE]Hourly Employee.");
+        }
+        // reads common properties
+        String name = inputMap.get("name");
+        Date hireDate = Date.valueOf(inputMap.get("hireDate"));
+
+        if("SE".equals(type)) {
+            Double salary = Double.valueOf(inputMap.get("salary"));
+
+            emp = new SalariedEmployee(name, hireDate, salary);
+
+        } else {
+            Double rate = Double.valueOf(inputMap.get("rate"));
+            Double hours = Double.valueOf(inputMap.get("hours"));
+
+            emp = new HourlyEmployee(name, hireDate, rate, hours);
+
+        }
 
         return emp;
     }

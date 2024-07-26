c1d199a678cfb10f4a69a60e8192ae3b29dc439b
Cortney B <cortneyb@tlgcohort.com>
2024-07-26 11:21:47 -0400

    anonymous class for test instantiation

--- Lab03.2/test/com/javatunes/personnel/EmployeeTest.java
+++ Lab03.2/test/com/javatunes/personnel/EmployeeTest.java
@@ -14,8 +14,28 @@ public class EmployeeTest {
 
     @Before
     public void setUp() {
-        emp = new SalariedEmployee("Cort", Date.valueOf("2024-05-07"));
-        emp2 = new SalariedEmployee("Cort", Date.valueOf("2024-05-07"));
+
+        emp = new Employee("Cort", Date.valueOf("2024-05-07")) {
+
+            @Override
+            public double payTaxes() {
+                return 0;
+            }
+
+            @Override
+            public double pay() {
+                return 0;
+            }
+        };
+        // creating anonymous (subclass) class bc cant create new Employee object (its abstract)
+        // you can clean it up so its not as messy by doing it inline
+        emp2 = new Employee("Cort", Date.valueOf("2024-05-07")) {
+            public double payTaxes() { return 0; }
+            public double pay() { return 0; }
+        };
+        // with mock
+        //emp = new DummyEmployee("Cort", Date.valueOf("2024-05-07"));
+        //emp2 = new DummyEmployee("Cort", Date.valueOf("2024-05-07"));
     }
 
     @Test
@@ -34,4 +54,28 @@ public class EmployeeTest {
         emp2.setName("Beans");
         assertNotEquals(emp, emp2);
     }
+
+
+
+    // NAMED MEMBER LEVEL INNER CLASS
+    // called a Mock, fake business type just for testing
+//    private class DummyEmployee extends Employee {
+//
+//        public DummyEmployee() {
+//        }
+//
+//        public DummyEmployee(String name, Date hireDate) {
+//            super(name, hireDate);
+//        }
+//
+//        @Override
+//        public double pay() {
+//            return 0;
+//        }
+//
+//        @Override
+//        public double payTaxes() {
+//            return 0;
+//        }
+//    }
 }
